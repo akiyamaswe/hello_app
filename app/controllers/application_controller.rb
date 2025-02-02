15f732bfed0ca96aafb695f0bbb812bfc640f78d
akiyama <ryo.akiyama112345@gmail.com>
2025-02-02 19:45:11 +0900

    goodbyeメソッドをrootに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "hello, world!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
